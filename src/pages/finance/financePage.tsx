da96091925f9b26fd840b093877550a946e602dc
Sudipto das <sudiptodas877@gmail.com>
2024-07-18 16:23:29 +0530

    added slider

--- src/pages/finance/financePage.tsx
+++ src/pages/finance/financePage.tsx
@@ -1,9 +1,15 @@
 import PartnerList from "../../components/finance-project/PartnerList"
-
+import Slider from "../../components/finance-project/slider";
+const data = [
+    'demo1.jpg',
+    'demo2.jpg',
+    'demo3.jpg',
+    
+];
 const FinancePage = () => {
     return (
-        <div className=" border rounded-lg p-6" style={{ height: 'calc(94vh - 88px)' }}>
-            <h1 className="text-3xl font-semibold mb-6 text-center md:text-start text-slate-500">Our Partners</h1>
+        <div className=" border rounded-lg p-6 min-h-screen flex flex-col " >
+            <Slider images={data} />
             <PartnerList />
         </div>
     )
---
+++ src/pages/project/projectDetailsPage.tsx
@@ -0,0 +1,6 @@
+const ProjectDetailsPage = () =>{
+    return (
+        <div>Project details Page ...</div>
+    )
+}
+export default ProjectDetailsPage
