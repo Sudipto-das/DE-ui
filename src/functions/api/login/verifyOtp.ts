99d2a8d304a78256950496ca03f1f91f53a962d3
Sudipto das <sudiptodas877@gmail.com>
2024-07-29 09:24:17 +0530

    added motion to usercard

--- src/functions/api/login/verifyOtp.ts
+++ src/functions/api/login/verifyOtp.ts
@@ -4,3 +4,19 @@ export const verifyOtp = async ({ otp, countryCode, phone}: { otp: string, count
     const response = await apiClient.post('/phone/verify-otp', { otp, countryCode, phone });
     return response.data;
 };
+
+export   const handleLogin = async ({
+    otp,
+    countryCode,
+    phone,
+  }: {
+    otp: string;
+    countryCode: string;
+    phone: string;
+  }) => {
+    const response = await verifyOtp({ otp, countryCode, phone });
+    if (response.status !== 200) {
+      throw new Error(response.message || "Server error");
+    }
+    return response.data;
+  };
