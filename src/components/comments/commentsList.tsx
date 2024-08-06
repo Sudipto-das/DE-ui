1bc0ac2f9bc3f612456ac625e5c9d0655ac234e3
Sudipto das <sudiptodas877@gmail.com>
2024-08-06 23:18:22 +0530

    fix deployment error

--- src/components/comments/commentsList.tsx
+++ src/components/comments/commentsList.tsx
@@ -9,39 +9,39 @@ import { projectRecIdState } from '../../store/projectsState/projectRecId';
 import Loader from '../ui/loader';
 import { projectDataState } from '../../store/projectsState/projectDataState';
 
-const dummyComments = [
-    {
-        Image: 'https://via.placeholder.com/40', // Replace with actual image URL
-        Title: 'Jane S',
-        CreatedBy: 'Maria Gomez',
-        Description: "I recently used your interior design services for my living room and I couldn't be happier! The team really listened to my needs and...",
-    },
-    {
-        Image: 'https://via.placeholder.com/40', // Replace with actual image URL
-        Title: 'John D',
-        CreatedBy: 'Maria Gomez',
-        Description: 'Working with your team was a breeze. They were professional, attentive, and really knew how to bring my vision to life. My home office...',
-    },
-    {
-        Image: 'https://via.placeholder.com/40', // Replace with actual image URL
-        Title: 'Tom H',
-        CreatedBy: 'Maria Gomez',
-        Description: "I've always struggled with making my small apartment feel cozy and inviting, but your interior design services completely transformed...",
-    },
-    {
-        Image: 'https://via.placeholder.com/40', // Replace with actual image URL
-        Title: 'John D',
-        CreatedBy: 'Maria Gomez',
-        Description: 'Working with your team was a breeze. They were professional, attentive, and really knew how to bring my vision to life. My home office...',
-    },
-    {
-        Image: 'https://via.placeholder.com/40', // Replace with actual image URL
-        Title: 'Tom H',
-        CreatedBy: 'Maria Gomez',
-        Description: "I've always struggled with making my small apartment feel cozy and inviting, but your interior design services completely transformed...",
-    },
-    // Add more comments as needed
-];
+// const dummyComments = [
+//     {
+//         Image: 'https://via.placeholder.com/40', // Replace with actual image URL
+//         Title: 'Jane S',
+//         CreatedBy: 'Maria Gomez',
+//         Description: "I recently used your interior design services for my living room and I couldn't be happier! The team really listened to my needs and...",
+//     },
+//     {
+//         Image: 'https://via.placeholder.com/40', // Replace with actual image URL
+//         Title: 'John D',
+//         CreatedBy: 'Maria Gomez',
+//         Description: 'Working with your team was a breeze. They were professional, attentive, and really knew how to bring my vision to life. My home office...',
+//     },
+//     {
+//         Image: 'https://via.placeholder.com/40', // Replace with actual image URL
+//         Title: 'Tom H',
+//         CreatedBy: 'Maria Gomez',
+//         Description: "I've always struggled with making my small apartment feel cozy and inviting, but your interior design services completely transformed...",
+//     },
+//     {
+//         Image: 'https://via.placeholder.com/40', // Replace with actual image URL
+//         Title: 'John D',
+//         CreatedBy: 'Maria Gomez',
+//         Description: 'Working with your team was a breeze. They were professional, attentive, and really knew how to bring my vision to life. My home office...',
+//     },
+//     {
+//         Image: 'https://via.placeholder.com/40', // Replace with actual image URL
+//         Title: 'Tom H',
+//         CreatedBy: 'Maria Gomez',
+//         Description: "I've always struggled with making my small apartment feel cozy and inviting, but your interior design services completely transformed...",
+//     },
+//     // Add more comments as needed
+// ];
 
 const CommentsList = () => {
     const ProjectRecIdState = useRecoilValue(projectRecIdState);
